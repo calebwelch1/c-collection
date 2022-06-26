bc7256755d4738b26c27770aa0a0abdf11862463
Bennington <caleb.welch11@gmail.com>
2022-06-26 15:34:25 -0400

    deploy

--- assets/AboutView.5f310201.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a,a as t,T as o}from"./index.f6b84d5f.js";const l={__name:"AboutView",setup(c){return(n,r)=>(e(),a("main",null,[t(o,{includeButton:!1})]))}};export{l as default};
---
+++ assets/linkedin.132bd5d6.js
@@ -0,0 +1,1 @@
+var s="/c-collection/assets/linkedin.b54ec892.svg";export{s as _};
